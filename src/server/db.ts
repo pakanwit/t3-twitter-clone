667c980f05d51c10b0334d5e26d8c10f9a295087
AppleClub <appleclub@AppleClubs-MacBook-Pro-2.local>
2024-08-31 18:01:37 +0700

    Update prisma schema with neon db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,23 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+// import dotenv from 'dotenv'
 
 import { env } from "~/env";
 
-const psClient = new Client({ url: env.DATABASE_URL });
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
 
+const pool = new Pool({ connectionString });
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const adapter = new PrismaNeon(pool);
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    adapter: adapter,
   });
 
 const globalForPrisma = globalThis as unknown as {
